d4ddc000c161837457ae2cfe4941b1950d70e9da
J Carlos <jcarlos@develoop.com>
2022-10-10 15:03:54 -0400

    add dessert form, from based template

---
+++ src/pages/jcarlos-form.tsx
@@ -0,0 +1,8 @@
+import JCarlosDessertContainer from 'src/containers/Landing/JCarlosDessertForm';
+import React, {memo} from 'react';
+
+const IndexPage = () => {
+  return <JCarlosDessertContainer />;
+};
+
+export default memo(IndexPage);

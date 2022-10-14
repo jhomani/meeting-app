7c64c5ebf091ef0e3812a625a15f8f94ad585a4f
evelin_solis <evelinsolis584@gmail.com>
2022-10-13 22:54:49 -0400

    interfaz de formulario  eve

---
+++ src/pages/Evelinindex.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {Formik, Form, Field} from 'formik';
+
+import './Evelinstyles.css';
+
+interface FormValues {
+  nombre_receta: string;
+  detalle_receta: string;
+  precio: Number;
+}
+
+const eveindex = (): JSX.Element => {
+  const initialValues: FormValues = {
+    nombre_receta: '',
+    detalle_receta: '',
+    precio: 0,
+  };
+
+  const onSubmit = (values: FormValues) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+    }, 500);
+  };
+
+  return (
+    <>
+      <h2>Formulario recetas</h2>
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        <Form className="form">
+          <label htmlFor="nombre_Receta" className="form__label">
+            First Name
+          </label>
+          <Field
+            name="nombre_recta"
+            type="text"
+            className="form__input"
+            placeholder="Nombre de receta"
+          />
+
+          <label htmlFor="detalle" className="form__label">
+            Last Name
+          </label>
+          <Field
+            name="detalle"
+            type="text"
+            className="form__input"
+            placeholder="Detalles"
+          />
+          <button type="submit" className="boton">
+            Verificar
+          </button>
+        </Form>
+      </Formik>
+    </>
+  );
+};
+
+export default eveindex;

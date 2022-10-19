c6e5ba7da731fb2ca6b92eee119e131464a373d2
GENKI <tianchizq@gmail.com>
2022-10-18 23:48:12 -0400

    creacion boton para generador de enlace

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,44 @@
+import {Button} from '@src/components';
+import {MeetRoomService} from '@src/services/room-service';
+import {useState} from 'react';
 
 const Home = () => {
+  const [roomCode, setCode] = useState();
+  const [roomName, setName] = useState('');
+
+  const onPressHandler = async () => {
+    const service = new MeetRoomService();
+
+    try {
+      const resp = await service.createRoom(roomName);
+      setCode(resp.roomCode);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <h2 className='txt-center mt-5'>Create New Room</h2>
+      <div className='create-room'>
+        {roomCode
+          ? <h6>{roomCode}</h6>
+          : <div className="input-group">
+              <label>Room Name:</label>
+              <input
+                type="text"
+                placeholder="clase"
+                name="name"
+                value={roomName}
+                onChange={(event) => setName(event.target.value)}
+              />
+          </div>
+        }
+        {!roomCode && <Button
+          content="Creat Room"
+          onPress={onPressHandler}
+        /> }
+      </div>
     </>
   );
 };
